e22edc48149ba52e4a0ea651160a47c184c8b8ab
mpho061 <monyamanempho13@gmail.com>
2021-08-16 19:19:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-kydpga
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-kydpga)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+import SignIn from './components/SignIn'; 
+import SignUp from './components/SignUp'; 
+import Cart from './components/Cart';
+import NavBar from './components/NavBar';
+import Shop from './components/Shop';
+
+export default function App() {
+  return (
+    <div className="App">
+    <NavBar></NavBar>        
+     </div>
+  );
+}
